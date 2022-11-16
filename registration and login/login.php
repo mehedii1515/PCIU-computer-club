3cda1fae51e80dad25f5e6dd55d87c4d069d997e
mehedi <mdmehedihasan1515@gmail.com>
2022-11-16 22:23:27 +0600

    all

--- registration and login/login.php
+++ registration and login/login.php
@@ -1,5 +1,5 @@
 <?php
-$email = $_POST['email'];
+$id = $_POST['id'];
 $pass = $_POST['pass'];
 
 //database connection
@@ -7,19 +7,19 @@ $con = new mysqli("localhost","root","","pciu computer club");
 if($con->connect_error){
     die("Failed to connect : ".$con->connect_error);
 }else{
-    $stmt = $con->prepare("select * from registration where email = ?");
-    $stmt-> bind_param("s",$email);
+    $stmt = $con->prepare("select * from register where id = ?");
+    $stmt-> bind_param("s",$id);
     $stmt->execute();
     $stmt_result = $stmt->get_result();
     if($stmt_result->num_rows > 0){
         $data = $stmt_result-> fetch_assoc();
-        if($data['password']===$password){
+        if($data['pass']===$pass){
             echo "<h2>Login Successfully</h2>";
         }else{
-            echo "<h2>Invalid Email or password</h2>";
+            echo "<h2>Invalid ID or password</h2>";
         }
     }else{
-        echo "<h2>Invalid email or password</h2>";
+        echo "<h2>Invalid ID or Password</h2>";
     }
 }
 ?>
