aa6761834575729b4e0db40389e3d493eeca2f39
mehedi <mdmehedihasan1515@gmail.com>
2022-12-22 11:19:04 +0600

    completed

---
+++ usermanagement/includes/leftbar.php
@@ -0,0 +1,12 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+			<li class="ts-label">Main</li>
+			<li><a href="profile.php"><i class="fa fa-user"></i> &nbsp;Profile</a>
+			</li>
+			
+			</ul>
+			<p class="text-center" style="color:#ffffff; margin-top: 100px;">© Md Mehedi Hasan</p>
+		</nav>
+
+		
